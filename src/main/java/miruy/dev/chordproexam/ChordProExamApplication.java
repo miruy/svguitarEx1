8e698179f3308e49b949d59d2856c50e4da7e46b
miruy <dbflarla4966@naver.com>
2024-09-28 21:19:18 +0900

    svguitar 설치 및 기본 적용 예제

---
+++ src/main/java/miruy/dev/chordproexam/ChordProExamApplication.java
@@ -0,0 +1,13 @@
+package miruy.dev.chordproexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChordProExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChordProExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/miruy/dev/chordproexam/ChordProExamApplicationTests.java
@@ -0,0 +1,13 @@
+package miruy.dev.chordproexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChordProExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
